3fc7063db5403ba62f55eb1a4fb1418c1ed970da
lilJ <julia.s@live.nl>
2025-12-08 16:48:35 +0100

    pages

---
+++ src/pages/Index.tsx
@@ -0,0 +1,21 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import MusicSection from "@/components/MusicSection";
+import AboutSection from "@/components/AboutSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <main className="min-h-screen bg-background">
+      <Navbar />
+      <HeroSection />
+      <MusicSection />
+      <AboutSection />
+      <ContactSection />
+      <Footer />
+    </main>
+  );
+};
+
+export default Index;
